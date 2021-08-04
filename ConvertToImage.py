7f7a9acd8e9e299115a5426502916f2585608b57
Ahmed TIJANI <72717112+ahmedtijaninet@users.noreply.github.com>
2021-08-04 07:48:07 +0100

    Add files via upload

---
+++ ConvertToImage.py
@@ -0,0 +1,27 @@
+from pdf2image import convert_from_path
+import os
+import sys
+import time
+
+def sources():
+    arr = os.listdir("C:/Users/Core/Desktop/data/source/1/")
+    source = "C:/Users/Core/Desktop/data/source/1/"
+    return arr,source
+arr, source = sources()
+
+def ConvertToImage(arr, source):
+    for arrs in arr :
+        pathname=arrs[:-4]
+        pathname=pathname.replace(" ", "")
+        outputDir = "source/"+pathname+'/'    
+        files=source+arrs
+        if not os.path.exists(outputDir):
+            os.makedirs(outputDir)
+        images = convert_from_path(files, 500)
+
+        for i, image in enumerate(images):
+            fname =str(i)+'.png'
+            image.save(outputDir+fname)
+ConvertToImage(arr, source)
+
+        
